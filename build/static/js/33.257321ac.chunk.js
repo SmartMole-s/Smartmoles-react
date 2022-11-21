554afdcd3fe9c2214041cbf564ba3e3b951de96b
Akın Turan <akin.turan@outlook.com.tr>
2022-11-21 22:23:08 +0300

    build

---
+++ build/static/js/33.257321ac.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@minimal/minimal-kit-react"]=this["webpackJsonp@minimal/minimal-kit-react"]||[]).push([[33],{733:function(i,a,t){"use strict";t.r(a);var m=t(950);a.default=m.a}}]);
+//# sourceMappingURL=33.257321ac.chunk.js.map
