ca0489e5cbe0e007ff5dccbedc3c2fa2cc60b07a
Akın Turan <akin.turan@outlook.com.tr>
2022-11-28 17:53:55 +0300

    Update Pdf.js

--- src/sections/home/Pdf.js
+++ src/sections/home/Pdf.js
@@ -1,9 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { useState } from 'react';
+import { Typography } from '@mui/material';
+
 
 export default function AboutPdf() {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
@@ -11,15 +14,19 @@ export default function AboutPdf() {
 
   return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-      {lang === 'tr' ? (
+      
         <div style={{ height: '720px' }}>
           <Viewer fileUrl={'/static/pdf/brosur.pdf'} plugins={[defaultLayoutPluginInstance]} />
         </div>
-      ) : (
+<br></br>
+<Typography variant='h3' sx={{y:10, textAlign:'center'}}>
+İngilizce Broşür
+</Typography>
+<br></br>
         <div style={{ height: '720px' }}>
           <Viewer fileUrl={'/static/pdf/brosur-ingilizce.pdf'} plugins={[defaultLayoutPluginInstance]} />
         </div>
-      )}
+
     </Worker>
   );
 }
